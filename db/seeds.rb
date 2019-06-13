87c47b6186b97ad794bf7389df40e9755eb5ede1
axionos <axionoso@gmail.com>
2019-06-13 09:19:39 -0400

    adjust the location and radius

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,23 @@ require 'rest-client'
 # lat = stores.map{|store| store['location']["lat"]}
 # lng = stores.map{|store| store['location']["lng"]}
 
-url1 = 'https://api.foursquare.com/v2/venues/explore?client_id=OCIOZ2I1T2ESD2QM5Q1PDDRIET1AIS5YHPIHKY52CXXCVVRZ&client_secret=ZGCJSC5AEZLR1IJXLBWMEG0FBL4AIVKKA55UKUBRW30DEU0B&v=20180323&ll=40.7243,-74.0018&query=bubble%20tea&limit=50'
+url = 'https://api.foursquare.com/v2/venues/explore?client_id=OCIOZ2I1T2ESD2QM5Q1PDDRIET1AIS5YHPIHKY52CXXCVVRZ&client_secret=ZGCJSC5AEZLR1IJXLBWMEG0FBL4AIVKKA55UKUBRW30DEU0B&v=20180323&ll=40.713579,-73.882671&query=bubble%20tea&limit=50&radius=16000'
 
-url2 = 'https://api.foursquare.com/v2/venues/explore?client_id=OCIOZ2I1T2ESD2QM5Q1PDDRIET1AIS5YHPIHKY52CXXCVVRZ&client_secret=ZGCJSC5AEZLR1IJXLBWMEG0FBL4AIVKKA55UKUBRW30DEU0B&v=20180323&ll=40.7243,-74.0018&query=bubble%20tea&limit=50&offset=50'
+
 
 def save_store_data(url)
 # FETCHING THE DATA
 response_string = RestClient.get(url)
 response_hash = JSON.parse(response_string)
 
-# ACCESSING TO EACH STORE HASH
+# ACCESSING EACH STORE HASH
 data = response_hash["response"]["groups"][0]["items"]
 stores = data.map{|store| store["venue"]}
+limit = response_hash["totalResults"]
+byebug
 
 # CREATING DATA INTO THE DATABASE
 stores.map{|store| Store.create(name: store["name"], img_url: "not found", address: store['location']["formattedAddress"].join(' '), lng: store['location']["lng"], lat: store['location']["lat"])}
 end
 
-save_store_data(url1)
-save_store_data(url2)
+save_store_data(url)
