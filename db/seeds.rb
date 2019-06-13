c0dd05e3d3a4c67e5e5f2c4a22a15f0e4b0ba7f7
Jacob Lam <jlam18@binghamton.edu>
2019-06-13 12:00:31 -0400

    all boba shop

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,36 @@ require 'rest-client'
 # lat = stores.map{|store| store['location']["lat"]}
 # lng = stores.map{|store| store['location']["lng"]}
 
-url = 'https://api.foursquare.com/v2/venues/explore?client_id=OCIOZ2I1T2ESD2QM5Q1PDDRIET1AIS5YHPIHKY52CXXCVVRZ&client_secret=ZGCJSC5AEZLR1IJXLBWMEG0FBL4AIVKKA55UKUBRW30DEU0B&v=20180323&ll=40.713579,-73.882671&query=bubble%20tea&limit=50&radius=16000'
-
+init_url = 'https://api.foursquare.com/v2/venues/explore?client_id=OCIOZ2I1T2ESD2QM5Q1PDDRIET1AIS5YHPIHKY52CXXCVVRZ&client_secret=ZGCJSC5AEZLR1IJXLBWMEG0FBL4AIVKKA55UKUBRW30DEU0B&v=20180323&ll=40.713579,-73.882671&query=bubble%20tea&limit=50&radius=16000'
 
 
 def save_store_data(url)
 # FETCHING THE DATA
 response_string = RestClient.get(url)
 response_hash = JSON.parse(response_string)
+# setting limit for while loop
+length = response_hash['response']["totalResults"] + 50
 
-# ACCESSING EACH STORE HASH
-data = response_hash["response"]["groups"][0]["items"]
-stores = data.map{|store| store["venue"]}
-limit = response_hash["totalResults"]
-byebug
 
-# CREATING DATA INTO THE DATABASE
-stores.map{|store| Store.create(name: store["name"], img_url: "not found", address: store['location']["formattedAddress"].join(' '), lng: store['location']["lng"], lat: store['location']["lat"])}
+
+counter = 0
+
+while counter < length
+
+  url = "https://api.foursquare.com/v2/venues/explore?client_id=OCIOZ2I1T2ESD2QM5Q1PDDRIET1AIS5YHPIHKY52CXXCVVRZ&client_secret=ZGCJSC5AEZLR1IJXLBWMEG0FBL4AIVKKA55UKUBRW30DEU0B&v=20180323&ll=40.713579,-73.882671&query=bubble%20tea&limit=50&radius=16000&offset=#{counter}"
+
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+
+  data = response_hash["response"]["groups"][0]["items"]
+  stores = data.map{|store| store["venue"]}
+  stores.map{|store| Store.create(name: store["name"], img_url: "not found", address: store['location']["formattedAddress"].join(' '), lng: store['location']["lng"], lat: store['location']["lat"])}
+
+  counter += 50
+
+end
+# # CREATING DATA INTO THE DATABASE
+#
 end
 
-save_store_data(url)
+save_store_data(init_url)
