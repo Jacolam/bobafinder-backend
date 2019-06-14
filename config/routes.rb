2b9bbe9f788a7db8a45926b8e43099847b47bcab
axionos <axionoso@gmail.com>
2019-06-14 09:55:12 -0400

    update stores api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/', to: 'application#welcome'
+  get '/stores', to: 'stores#index'
   post '/login', to: 'auth#login'
   post '/signup', to: "users#create"
 
