2f05188bf42d0329e29431873f236a315c90cacc
Jacob Lam <jlam18@binghamton.edu>
2019-06-16 14:59:51 -0400

    can visit and unvisit locations

--- app/controllers/user_store_controller.rb
+++ app/controllers/user_store_controller.rb
@@ -3,7 +3,10 @@ class UserStoreController < ApplicationController
   def visit
     store_id = params["store_id"].to_i
     user_id = current_user.id
-    new = UserStore.find_or_create_by(user_id: user_id, store_id: store_id, visited: true )
+    new = UserStore.find_or_create_by(user_id: user_id, store_id: store_id)
+    new["visited"] = !new["visited"]
+    # allows us to visit and unvisit locations 
+    new.save
     render json:{visited:new }
   end
 
