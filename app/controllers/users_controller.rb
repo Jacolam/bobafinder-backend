abeb1e46da76f16fea9127438bf38d8e7b5f5ee3
Jacob Lam <jlam18@binghamton.edu>
2019-06-18 17:08:04 -0400

    fixed i think

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,14 +17,16 @@ class UsersController < ApplicationController
     favorite_selected = current_user.user_stores.select { |store| store.favorite == true}
 
     # FIND STORE IDS FOR THE FAVORITE STORES
-    store_ids = favorite_selected.map {|x| x.id}
+    fav_ids = favorite_selected.map{ |fav| fav.store_id}
 
     # RENDER FAVORITE STORE INFORMATION
-    favorites = store_ids.map { |num| current_user.stores.find(num) }
+    favorites = fav_ids.map { |num| current_user.stores.find_by(id: num) }
 
     visited_selected = current_user.user_stores.select { |store| store.visited == true}
-    store_ids = visited_selected.map {|x| x.id}
-    visited = store_ids.map { |num| current_user.stores.find(num) }
+
+    visit_ids = visited_selected.map {|x| x.store_id}
+
+    visited = visit_ids.map { |num| current_user.stores.find_by(id: num) }
     render json: {favorites: favorites, visited: visited, username:current_user.username}
   end
 
